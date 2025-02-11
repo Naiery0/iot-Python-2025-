eee77b726dfe3323c51dfd44626cf4039eb7ace4
Naiery0 <molly724@naver.com>
2025-02-11 17:00:41 +0900

    2일차 커밋 내용 수정

--- day02/py05_operator.py
+++ day02/py05_operator.py
@@ -1,30 +1,32 @@
 # py05_operator.py
 # 연산자
 
-# 사칙연산 +, -, *, /
-a, b = 5, 3
-# tip : Shift + Del -> 한줄 삭제
-
+# 사칙연산 : + - * /
+a, b = 15, 14
+# Shift + Del 는 한줄 삭제(매우 효율!)
 print(a + b)
 print(a - b)
 print(a * b)
 
-# 나누기 /, //, %
-print(a / b) # 나눈 결과는 float
-print(a // b) # 몫 int 
-print(a % b) # 나머지 int
+# 나누기 연산자 /, //, %
+a = 14
+b = 4
+print(a / b)  # 나눈 결과는 float
+print(a // b) # 나눈 몫, int
+print(a % b)  # 나머지, int
 
 # 거듭제곱(Power) **
-print(2 ** 5)
+print(2 ** 10)
 
 # 연산자 우선순위
-## 계산식이 복잡하여 우선순위를 모르겠으면 ()
+## 계산식이 복잡해서 연산자 우선순위를 잘 모르겠으면 () 사용
 print((3 + 4) * 7)
 print(3 + (4 * 7))
 
+
 ## 리스트연산
-## last index = len(list)-1
-listSample = [1, 3, 5, 7, 9]
+## last index = len(list) - 1
+listSample = [1,3,5,7,9]
 print(listSample[0])
 print(len(listSample)) # 리스트의 길이
 
@@ -32,34 +34,56 @@ print(listSample[1])
 print(listSample[2])
 print(listSample[3])
 print(listSample[4])
-print(listSample[len(listSample)-1])
 
-## 문자열 연산
+listSample[4] = 11
+
+print(listSample[len(listSample) - 1])
+# print(listSample[5])
+
+## 문자열 연산 : +, * 만 존재
 greeting = 'Hello'
-target = 'world'
-print(greeting, target)
-
-print(greeting + target) # string concatenate
+target = 'World'
+print(greeting, target) # 문자열 연산 X
+print(greeting + ' ' + target) # 문자열 연산 + string concatenate
 print('{0} {1}'.format(greeting, target))
 print(f'{greeting} {target}')
 
-print(greeting * 5) # 해당 문자열을 * 수로 반복
+print(greeting * 5) # 해당문자열을 * 수로 반복
 
-listSample = ['2','0','2','5','-','0','2','-','0','4']
+## 문자열(Charactor Array) : List와 유사하지만 값 수정 불가
+print(greeting[1]) # 리스트 연산 
+# greeting[0] = 'C'
+print(greeting)
+
+## 리스트연산, 슬라이싱
+listSample = ['2', '0', '2', '5', '-', '0', '2', '-', '0', '4']
 current = '2025-02-04'
 
-# 슬라이싱, 리스트를 자르기
-print(listSample[0:3]) # 인덱스 3번 앞까지만. (0~2번까지 출력)
+for i in listSample:
+    print(i, end='')
+print()
 
+print(current)
+# 준비 끝
+
+# 인덱싱, 인덱스에 있는 값을 가져오기
+print(listSample[-7])
+print(current[-7])
+
+# 슬라이싱, 리스트를 자르기
+## [start:end] : end - 1까지만 추출
+print(listSample[0:3 + 1])
+print(current[0:3 + 1])
 
 # 2025-02-04
 year = current[0:3 + 1]
 month = current[5:6 + 1]
-day = current[8:] # 끝까지
-print (year, month, day)
+day = current[8:] # end 끝가지는 숫자 생략
+print(year, month, day)
+print(current[-2:])
 
-## 문자열 연산 중 함수 사용
-full_name = "Hugo Mg. Sung"
+## 문자열 연산 중 함수를 사용
+full_name = "Hugo MG. Sung"
 # 자르기
 print(full_name.split())
 print(full_name.split(' '))
@@ -73,18 +97,29 @@ print(type(names))
 print(names)
 
 # 바꾸기
-print(full_name.replace('Hugo', 'Ashley'))
+print(full_name.replace('Hugo MG.', 'Ashley'))
 
 # 공백제거
-origin = '     hello  ~     '
-print(f'//{origin.lstrip()}//') # 좌 공백제거
-print(f'//{origin.rstrip()}//') # 우 공백제거
-print(f'//{origin.strip()}//') # 좌우 공백제거
+origin = '     Hello  ~     '
+print(f'//{origin}//')
+print(f'//{origin.lstrip()}//')
+print(f'//{origin.rstrip()}//')
+print(f'//{origin.strip()}//')
 
 # 단어찾기
-full_name = "Hugo Mg. Sung"
-print(full_name.find('g')) # 찾은 첫 번째 인덱스, 없으면 -1 출력
-print(full_name.count('g')) # g가 문장에 몇 개 존재하는가?
-#print(full_name.index('h')) # 오류 남.
-print(full_name.upper()) #대문자
-print(full_name.lower()) #소문자
+full_name = "Hugo MG. Sung"
+print(full_name.find('G'))
+print(full_name.find('g'))
+print(full_name.find('h'))  # -1 : h를 찾을 수 없음!
+
+print(full_name.count('g')) # g가 문장에 몇번 존재
+# print(full_name.index('h')) # 오류발생!
+
+print(full_name.upper()) 
+print(full_name.lower())
+
+## T로 자를때
+# '', "" == Empty(비어있다)
+# ' ', " " == Space(공백존재)
+origin = 'ITESTSTRING'
+print(origin.split('T'))
